d299ceca80b04e16ba8b94f6b301c69e2e5a1243
noizzer28 <colosova28@yandex.ru>
2023-03-31 19:27:23 +0400

    hw-3 corrections

--- JS/hw-3.js
+++ JS/hw-3.js
@@ -33,26 +33,26 @@ alert(a + b);
 
 // 5
 
-let monthNumber =  prompt("Enter month`s number")
+let monthNumber =  Number(prompt("Enter month`s number"));
 switch (monthNumber) {
-    case ("1"):
-    case ("2"):
-    case ("12"):
+    case 1:
+    case 2:
+    case 12:
         console.log ("Winter")
         break;
-    case ("3"):
-    case ("4"):
-    case ("5"):
+    case 3:
+    case 4:
+    case 5:
         console.log ("Spring")
         break;
-    case ("6"):
-    case ("7"):
-    case ("8"):
+    case 6:
+    case 7:
+    case 8:
         console.log ("Summer")
         break;
-    case ("9"):
-    case ("10"):
-    case ("11"):
+    case 9:
+    case 10:
+    case 11:
         console.log ("Autumn")
         break;
     default:
